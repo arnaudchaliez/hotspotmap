ac878fa4bf45be6cdadd49c4318be378af600eb8
Jérémy Bouny <jbouny@gmail.com>
2014-02-12 16:34:32 +0100

    Base of the project with Silex. Update the vagrant configuration.

---
+++ web/index.php
@@ -0,0 +1,11 @@
+﻿<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app->get('/', function () {
+    return "Hello World.";
+});
+
+// definitions
+$app['debug'] = true;
+$app->run();
